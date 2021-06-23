9c5839bdf98c14e6f897d7ec862d9cafcb037722
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-06-23 10:43:50 +0000

    Echo nextcloud wait message to systemd-cat.

--- sh/nextcloud-background.sh
+++ sh/nextcloud-background.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while ! $(secret-tool lookup Title "Nextcloud Marc" &> /dev/null); do
-	echo "wait"
-	sleep 1s
+	echo "Nexcloud secret not available. Trying again in 1 min..." | systemd-cat
+	sleep 60s
 done
 
 /usr/bin/nextcloud --background
