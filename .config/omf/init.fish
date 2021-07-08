31e10eaba0ac380b3f2d471d504512c0c10eef3e
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-07-08 09:03:53 +0000

    Correct mbsync alias.

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -17,7 +17,7 @@ alias vim="nvim"
 alias te="nvim -c \":te\""
 alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 alias win10="./$HOME/sh/wi10.sh"
-alias mbsync="mbsync -c \"$XDG_CONFIG_HOME/isync/mbsyncrc\""
+alias mbsync="mbsync -c $XDG_CONFIG_HOME/isync/mbsyncrc"
 alias nvim-rebuild="rm ~/.cache/paru/clone/neovim-nightly-bin/ -rf && paru --rebuild --redownload neovim-nightly-bin"
 
 begin
