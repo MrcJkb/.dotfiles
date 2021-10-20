f4b012bcc32f70e5a60cad5bd835b84f7c4c7475
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-10-20 19:07:46 +0200

    Add cfilter plugin

--- sh/win10.sh
+++ sh/win10.sh
@@ -13,15 +13,17 @@ qemu-system-x86_64 -enable-kvm \
         -device intel-hda -device hda-output,audiodev=snd0 \
         -rtc base=localtime,clock=host \
         -smp cores=2,threads=4 \
+        -device virtio-serial-pci \
+        -spice port=${SPICE_PORT},disable-ticketing=on \
+        -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
+        -chardev spicevmc,id=spicechannel0,name=vdagent
         "$@"
-        # -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-        # -cdrom ${HOME}/virtual-machines/virtio-win.iso \
-        # TODO: Add SPICE support, see https://wiki.archlinux.org/title/QEMU#Enabling_SPICE_support_on_the_host
-        # -spice port=${SPICE_PORT},disable-ticketing=on \
-        # -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-        # -chardev spicevmc,id=spicechannel0,name=vdagent
 # To boot from a gparted live CD, uncomment the below lines and add them as exec parameters
         # -boot d \
         # -cdrom $HOME/Downloads/gparted-live-1.3.0-1-i686.iso \
 
-# exec spicy --title Windows 127.0.0.1 -p ${SPICE_PORT}
+exec spicy --title Windows 127.0.0.1 -p ${SPICE_PORT}
+
+        # -bios /usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
+        # -cdrom ${HOME}/virtual-machines/virtio-win.iso \
+        # TODO: Add SPICE support, see https://wiki.archlinux.org/title/QEMU#Enabling_SPICE_support_on_the_host
