05a4139db45c40a012e800d668a9a1ba585b25d5
Marc Jakobi <marc.jakobi@tiko.energy>
2022-02-01 23:20:40 +0100

    Source z.lua depending on distro

--- .config/fish/conf.d/z.fish
+++ .config/fish/conf.d/z.fish
@@ -1,2 +1,9 @@
-# Assumes the AUR z.lua package has been installed
-# source /usr/share/z.lua/init.fish
+set unamea (uname -a)
+
+switch $unamea
+  case "*arch*" # Arch Linux
+    # Assumes the AUR z.lua package has been installed
+    source /usr/share/z.lua/init.fish
+  case "*nixos*" # NixOS
+    z --init fish | source
+  end
