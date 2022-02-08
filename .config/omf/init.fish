65899b4c347e59d4ea30bf2908ac99983c9f575c
Marc Jakobi <marc.jakobi@tiko.energy>
2022-02-08 02:30:57 +0100

    Fish/OMF fixes

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -15,19 +15,19 @@ end
 
 fish_vi_key_bindings
 
-# Start X at login
-if status is-login
-  if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    # xrandr --newmode "5120x1440_30.00"  293.23  5120 5360 5896 6672  1440 1441 1444 1465  -HSync +Vsync
-    # xrandr --addmode DP1 "5120x1440_30.00"
-    exec startx -- -keeptty
-  end
-  # Set keymap to us altgr-intl if Atreus is connected
-  set -l atreus_output (lsusb | grep Atreus)
-  if test -n "$atreus_output"
-    setxkbmap us altgr-intl
-  end
-end
+# # Start X at login
+# if status is-login
+#   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+#     # xrandr --newmode "5120x1440_30.00"  293.23  5120 5360 5896 6672  1440 1441 1444 1465  -HSync +Vsync
+#     # xrandr --addmode DP1 "5120x1440_30.00"
+#     exec startx -- -keeptty
+#   end
+#   # Set keymap to us altgr-intl if Atreus is connected
+#   set -l atreus_output (lsusb | grep Atreus)
+#   if test -n "$atreus_output"
+#     setxkbmap us altgr-intl
+#   end
+# end
 
 bass source $HOME/.profile
 #bass source $HOME/.sdkman/bin/sdkman-init.sh
@@ -35,7 +35,7 @@ set --local NIX_DAEMON = /etc/profile.d/nix{,-daemon}.sh
 if test -e "$NIX_DAEMON"
   bass source "$NIX_DAEMON"
 end
-set fish_greeting ""
+# set fish_greeting ""
 
 # Starship theme (requires starship)
 starship init fish | source
