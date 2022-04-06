25988df6cfae413fabf2ae2d5334dde91304e196
Marc Jakobi <marc.jakobi@tiko.energy>
2022-04-06 11:41:22 +0200

    Update nextcloud.sh

--- sh/nextcloud.sh
+++ sh/nextcloud.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-while ! $(secret-tool lookup Title "Nextcloud Marc" &> /dev/null); do
+# See https://github.com/nextcloud/desktop/issues/2303#issuecomment-708913062
+# and https://www.cogitri.dev/posts/03-keepassxc-freedesktop-secret/
+
+# 1 Configure KeePassXC + DB for Secret Service Integration
+# 2 Add this script to autostart
+
+while ! $(secret-tool lookup Title "TheGoodCloud (NextCloud)" &> /dev/null); do
 	echo "wait"
 	sleep 1s
 done
-
-/usr/bin/nextcloud # --background
+nextcloud # --background
