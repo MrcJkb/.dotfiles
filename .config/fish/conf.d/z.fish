4085fbd69e01826cf5dac9a36a30a8f0312f921e
Marc Jakobi <marc.jakobi@tiko.energy>
2022-08-08 16:18:16 +0200

    Delete z.fish

--- .config/fish/conf.d/z.fish
+++
@@ -1,9 +0,0 @@
-set unamea (uname -a)
-
-switch $unamea
-  case "*arch*" # Arch Linux
-    # Assumes the AUR z.lua package has been installed
-    source /usr/share/z.lua/init.fish
-  case "*nixos*" # NixOS
-    z --init fish | source
-  end
